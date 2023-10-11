8102d7ea6dceb09a55b6bbe14be32348c2e80175
shanif <97924668+salamandur@users.noreply.github.com>
2023-10-10 22:44:57 -0400

    x3

--- App/controllers/competition.py
+++ App/controllers/competition.py
@@ -1,9 +1,20 @@
-from App.controllers.__init__ import ensure_admin
+from functools import wraps
+from flask_jwt_extended import jwt_required, current_user
 from flask_restful import Resource
 from flask import jsonify, request, make_response
 from flask_jwt_extended import current_user
 from App.models import Competition,User, UserCompetition
 from App.database import db
+
+def ensure_admin(u):
+    @wraps(u)
+    @jwt_required()
+    def decorated_function(*args, **kwargs):
+        user = current_user
+        if user and user.type == 'admin':
+            return u(*args, **kwargs)
+        return make_response(jsonify(error="Unauthorized"), 401)
+    return decorated_function
 
 class CompetitionController(Resource):
     decorators = [ensure_admin]
